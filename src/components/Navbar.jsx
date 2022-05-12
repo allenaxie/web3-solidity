8fc03cf592ff264adee0e4713f10d6f12be2dfde
allenaxie <allenaxie1@gmail.com>
2022-05-11 18:48:53 -0700

    Create welcome text and grids.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,13 +34,12 @@ const Navbar = () => {
                     : <HiMenuAlt4 fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(true)} />
                 }
                 {toggleMenu && (
-                    <ul
+                    <ul 
                         className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2x1 md:hidden list-none
-                        flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in
-                        "
+                        flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
                     >
                         <li className='text-xl w-full my-2'>
-                            <AiOutlineClose onClick={() => setToggleMenu(false)} />
+                            <AiOutlineClose onClick={() => setToggleMenu(false)} className='cursor-pointer'/>
                         </li>
                         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
                             <NavbarItem key={item + index} title={item} classProps="my-2 text-lg"/>
