ab983407ce9359372010b079eb663fd45d7d137a
allenaxie <allenaxie1@gmail.com>
2022-05-17 01:46:12 -0700

    Import dummy data for transactions.

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { TransactionContext } from '../context/TransactionContext';
+
+import dummyData from '../utils/dummyData';
 
 const Transactions = () => {
+  const {currentAccount} = useContext(TransactionContext);
+
   return (
-    <div>Transactions</div>
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+        {/* Latest Transactions */}
+        {/*  Connect your account to see the latest transactions*/}
+      </div>
+    </div>
   )
 }
 
