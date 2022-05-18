cf596a4339eba3fe487deb6ef01762a63f063a50
allenaxie <allenaxie1@gmail.com>
2022-05-17 17:22:56 -0700

    Create custom hook- useFetch and used in Transactions component.

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -2,8 +2,12 @@ import React, { useContext } from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 import { shortenAddress } from '../utils/shortenAddress';
 import dummyData from '../utils/dummyData';
+import useFetch from '../hooks/useFetch';
 
-const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
+const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+
+  const gifUrl = useFetch({ keyword });
+
   return (
     <div className='bg-[#181918] m-4 flex flex-1
       2xl:min-w-[450px]
@@ -27,6 +31,21 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
           <p className='text-white text-base'>
             Amount: {amount} ETH
           </p>
+          {message && (
+            <>
+              <br />
+              <p className='text-white text-base'>
+                Message: {message}
+              </p>
+            </>
+          )}
+
+        </div>
+        <img src={gifUrl || url} alt="gif" className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover" />
+        <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+          <p className='text-[#37c7da] font-bold'>
+            {timestamp}
+          </p>
         </div>
       </div>
     </div>
@@ -34,28 +53,28 @@ const TransactionCard = ({ addressTo, addressFrom, timestamp, message, keyword, 
 }
 
 const Transactions = () => {
-  const {currentAccount} = useContext(TransactionContext);
+  const { currentAccount } = useContext(TransactionContext);
 
   return (
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
       <div className="flex flex-col md:p-12 py-12 px-4">
         {currentAccount ?
-        (
-        <h3 className="text-white text-3xl text-center my-2 ">
-          Latest Transactions
-        </h3>
-        )
-        :
-        (
-        <h3 className="text-white text-3xl text-center my-2 ">
-          Connect your account to see the latest transactions
-        </h3>
-        )}
+          (
+            <h3 className="text-white text-3xl text-center my-2 ">
+              Latest Transactions
+            </h3>
+          )
+          :
+          (
+            <h3 className="text-white text-3xl text-center my-2 ">
+              Connect your account to see the latest transactions
+            </h3>
+          )}
 
         <div className="flex flex-wrap justify-center items-center mt-10">
           {dummyData.reverse().map((transaction, index) => (
-            <TransactionCard key={index} 
-            {...transaction} //passes all props *hover to view*
+            <TransactionCard key={index}
+              {...transaction} //passes all props *hover to view*
             />
           ))}
         </div>
